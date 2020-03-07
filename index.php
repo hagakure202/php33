320a3fb94217f1da7749a5175ffcde931e4ae0da
Test <Seolab.dp.ua@gmail.com>
2020-03-07 10:43:56 +0200

    remove one task

--- index.php
+++ index.php
@@ -17,19 +17,7 @@ $x = buyBread(50);
 $x = buyBread('',3);
 $x = buyBread(50,3);
 echo $x;
-//----------------------------
-for ($i=0; $i<12; $i++){	
-	$a[$i] = rand (1,10);
-	$b[$i] = rand (1,10);
-}
-		
-	var_dump($a, $b);
-	$m=[];
-		for ($i=0; $i<12; $i++){
-			for ($j=0; $j<12; $j++){
-				 echo $a[$i][$j]**2*$b[$i][$j];
-			}
-		}
+
  ?>
 
 </body>
